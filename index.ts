dafe96016361e061d1468f7f7bdb62654e93ed54
Gary Becks <techstar.dev@hotmail.com>
2020-08-05 20:24:30 -0400

    add 64 bit int types

--- index.ts
+++ index.ts
@@ -4,18 +4,20 @@
  */
 
 
-export type Bool = boolean & {readonly brand: unique symbol}
+export type Bool = number & {readonly brand: unique symbol}
 export type Int8 = number & {readonly brand: unique symbol}
 export type Uint8 = number & {readonly brand: unique symbol}
 export type Int16 = number & {readonly brand: unique symbol}
 export type Uint16 = number & {readonly brand: unique symbol}
 export type Int32 = number & {readonly brand: unique symbol}
 export type Uint32 = number & {readonly brand: unique symbol}
+export type Int64 = number & {readonly brand: unique symbol}
+export type Uint64 = number & {readonly brand: unique symbol}
 export type Float32 = number & {readonly brand: unique symbol}
 export type Float64 = number & {readonly brand: unique symbol}
 export type Null = null & {readonly brand: unique symbol}
 export type String = string & {readonly brand: unique symbol}
-export type Err = Error & {readonly brand: unique symbol}
+export type Err = {} & {readonly brand: unique symbol}
 // will actually be any in generated typescript code
 export type Any = unknown & {readonly brand: unique symbol}
 export type TimeStamp = Date & {readonly brand: unique symbol}
@@ -28,4 +30,6 @@ export type Tuple5<T extends RpcType = Any,R extends RpcType = Any,S extends Rpc
 export type List<T extends RpcType = Any> = Array<T> & {readonly brand: unique symbol}
 
 
-export type RpcType = Bool | Int8 | Uint8 | Int16 | Uint16 | Int32 | Uint32 | Float32 | Float64 | Null | String | Err | Dict | Tuple2 | Tuple3 | Tuple4 | Tuple5 | List | Any | TimeStamp | Blob
+export type RpcType = Bool | Int8 | Uint8 | Int16 | Uint16 | Int32 | Uint32 | Int64 | Uint64 | Float32 | Float64 | Null | String | Err | Dict | Tuple2 | Tuple3 | Tuple4 | Tuple5 | List | Any | TimeStamp | Blob
+
+
