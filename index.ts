96e47ec7d61d200491deb5c6fa911a4dd5051024
Gary Becks <techstar.dev@hotmail.com>
2020-08-05 21:30:14 -0400

    fix types for codegen

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /*
-* defines the type system for
+* defines the type system for typerpc
 *
  */
 
@@ -18,16 +18,15 @@ export type Float64 = number & {readonly brand: unique symbol}
 export type Null = null & {readonly brand: unique symbol}
 export type String = string & {readonly brand: unique symbol}
 export type Err = {} & {readonly brand: unique symbol}
-// will actually be any in generated typescript code
 export type Any = unknown & {readonly brand: unique symbol}
 export type TimeStamp = Date & {readonly brand: unique symbol}
 export type Blob = Uint8Array & {readonly brand: unique symbol}
-export type Dict<T extends RpcType = Any,S extends RpcType = Any> = Map<T,S> & {readonly brand: unique symbol}
-export type Tuple2<T extends RpcType = Any,X extends RpcType = Any> = [T,X] & {readonly brand: unique symbol}
-export type Tuple3<T extends RpcType = Any,R extends RpcType = Any,S extends RpcType = Any> = [T,R,S] & {readonly brand: unique symbol}
-export type Tuple4<T extends RpcType = Any,R extends RpcType = Any,S extends RpcType = Any, U extends RpcType = Any> = [T,R,S, U] & {readonly brand: unique symbol}
-export type Tuple5<T extends RpcType = Any,R extends RpcType = Any,S extends RpcType = Any ,U extends RpcType = Any,V extends RpcType = Any> = [T,R,S,U,V] & {readonly brand: unique symbol}
-export type List<T extends RpcType = Any> = Array<T> & {readonly brand: unique symbol}
+export type Dict<T extends RpcType = Any,S extends RpcType = Any> = {keyType: T, valueType: S} & {readonly brand: unique symbol}
+export type Tuple2<T extends RpcType = Any,X extends RpcType = Any> = {item1: T, item2: X} & {readonly brand: unique symbol}
+export type Tuple3<T extends RpcType = Any,R extends RpcType = Any,S extends RpcType = Any> = {item1: T, item2: R, item3: S} & {readonly brand: unique symbol}
+export type Tuple4<T extends RpcType = Any,R extends RpcType = Any,S extends RpcType = Any, U extends RpcType = Any> = {item1: T, item2: R, item3: S, item4: U} & {readonly brand: unique symbol}
+export type Tuple5<T extends RpcType = Any,R extends RpcType = Any,S extends RpcType = Any ,U extends RpcType = Any,V extends RpcType = Any> = {item1: T, item2: R, item3: S, item4: U, item5: V} & {readonly brand: unique symbol}
+export type List<T extends RpcType = Any> = {elemType: T} & {readonly brand: unique symbol}
 
 
 export type RpcType = Bool | Int8 | Uint8 | Int16 | Uint16 | Int32 | Uint32 | Int64 | Uint64 | Float32 | Float64 | Null | String | Err | Dict | Tuple2 | Tuple3 | Tuple4 | Tuple5 | List | Any | TimeStamp | Blob
