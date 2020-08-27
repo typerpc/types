dc98034d588b6e65e298d3fa2c894b5c8bd62186
Gary Becks <techstar.dev@hotmail.com>
2020-08-27 09:08:02 -0400

    add QuerySvc and MutationSvc types

--- index.ts
+++ index.ts
@@ -4,54 +4,54 @@
  */
 
 /* eslint-disable @typescript-eslint/no-namespace */
-type typeRpcPrimitive = { readonly brand: unique symbol }
+type scalar = { readonly brand: unique symbol }
 
-type nonComparablePrimitive = { readonly brand: unique symbol }
-type typeRpcContainer = { readonly brand: unique symbol }
+type nonComparableScalar = { readonly brand: unique symbol }
+type container = { readonly brand: unique symbol }
 
-type primitiveOrUndefined = typeRpcPrimitive | undefined
-type containerOrUndefined = typeRpcContainer | undefined
-type messagable = primitiveOrUndefined | containerOrUndefined
+type scalarOrUndefined = scalar | undefined
+type containerOrUndefined = container | undefined
+type messagable = scalarOrUndefined | containerOrUndefined
 
 export namespace $ {
     // Primitive internal
     import Paramable = internal.Paramable
     import Comparable = internal.Comparable
-    export type bool = { toString(): string } & typeRpcPrimitive
-    export type int8 = { toString(): string } & typeRpcPrimitive
-    export type uint8 = { toString(): string } & typeRpcPrimitive
-    export type int16 = { toString(): string } & typeRpcPrimitive
-    export type uint16 = { toString(): string } & typeRpcPrimitive
-    export type int32 = { toString(): string } & typeRpcPrimitive
-    export type uint32 = { toString(): string } & typeRpcPrimitive
-    export type int64 = { toString(): string } & typeRpcPrimitive
-    export type uint64 = { toString(): string } & typeRpcPrimitive
-    export type float32 = { toString(): string } & typeRpcPrimitive
-    export type float64 = { toString(): string } & typeRpcPrimitive
-    export type str = { toString(): string } & typeRpcPrimitive
-    export type err = { toString(): string } & typeRpcPrimitive
-    export type timestamp = { toString(): string } & typeRpcPrimitive
+    export type bool = 'bool' & { toString(): string } & scalar
+    export type int8 = 'int8' & { toString(): string } & scalar
+    export type uint8 = 'uint8' & { toString(): string } & scalar
+    export type int16 = 'int16' & { toString(): string } & scalar
+    export type uint16 = 'uint16' & { toString(): string } & scalar
+    export type int32 = 'int32' & { toString(): string } & scalar
+    export type uint32 = 'uint32' & { toString(): string } & scalar
+    export type int64 = 'int64' & { toString(): string } & scalar
+    export type uint64 = 'uint64' & { toString(): string } & scalar
+    export type float32 = 'float32' & { toString(): string } & scalar
+    export type float64 = 'float64' & { toString(): string } & scalar
+    export type str = 'str' & { toString(): string } & scalar
+    export type err = 'err' & { toString(): string } & scalar
+    export type timestamp = 'timestamp' & { toString(): string } & scalar
     /**
      * Uint8Array in Js. []byte in Go.
      */
-    export type blob = { toString(): string } & typeRpcPrimitive
+    export type blob = 'blob' & { toString(): string } & scalar
     /**
      * any type in Ts/Rust, interface{} in go, dynamic in C#/Dart
      */
-    export type dyn = { toString(): string } & typeRpcPrimitive
+    export type dyn = 'dyn' & { toString(): string } & scalar
 
     // Primitives, but can't be used as keys for anything
     /**
      * represents the absence of a value. void in C derived most languages.
      */
-    export type unit = { toString(): string } & nonComparablePrimitive
-    export type nil = { toString(): string } & nonComparablePrimitive
+    export type unit = 'unit' & { toString(): string } & nonComparableScalar
+    export type nil = 'nil' & { toString(): string } & nonComparableScalar
     export type Dict<T extends Comparable, S extends Paramable> = Readonly<{
         keyType: T
         valType: S
         toString(): string
     }> &
-        typeRpcContainer
+        container
     /**
      * Do not use Tuples as method parameters when generating Go Code.
      * Can only be used as return type.
@@ -61,7 +61,7 @@ export namespace $ {
         item2: X
         toString(): string
     }> &
-        typeRpcContainer
+        container
     /**
      * Do not use Tuples as method parameters when generating Go Code.
      * Can only be used as return type.
@@ -72,7 +72,7 @@ export namespace $ {
         item3: S
         toString(): string
     }> &
-        typeRpcContainer
+        container
     /**
      * Do not use Tuples as method parameters when generating Go Code.
      * Can only be used as return type.
@@ -84,7 +84,7 @@ export namespace $ {
         item4: U
         toString(): string
     }> &
-        typeRpcContainer
+        container
     /**
      * Do not use Tuples as method parameters when generating Go Code.
      * Can only be used as return type.
@@ -103,26 +103,36 @@ export namespace $ {
         item5: V
         toString(): string
     }> &
-        typeRpcContainer
+        container
 
     export type List<T extends Paramable> = Readonly<{
         dataType: T
         toString(): string
     }> &
-        typeRpcContainer
+        container
 }
 
 export namespace rpc {
     import RpcType = internal.RpcType
-    type Func = (...params: (internal.RpcType | undefined)[]) => RpcType
+    import QueryParamable = internal.QueryParamable
+
+    type QueryFunc = (...params: (QueryParamable | undefined)[]) => RpcType
+    type MutationFunc = (...params: (internal.Paramable | undefined)[]) => RpcType
 
     /**
-     * Constructs a new typerpc Service definition.
-     * Only valid typerpc internal can be used as parameters and return
-     * internal.
+     * A service that can handle mutations by using HTTP POST requests.
+     *
+     * Only valid typerpc QueryParamable types and rpc.Msg can be used as parameters
+     * the return type can be any typerpc type or rpc.Msg
      */
-    export type Service<T extends { [key: string]: Func }> = T
+    export type MutationSvc<T extends { [key: string]: MutationFunc }> = T
 
+    /** A service that can handle queries by using HTTP Get requests.
+     *
+     * Only valid typerpc Paramable types and rpc.Msg can be used as parameters
+     * the return type can be any typerpc type or rpc.Msg
+     */
+    export type QuerySvc<T extends { [key: string]: QueryFunc }> = T
     /**
      * Used to construct a type alias a typerpc Type alias.
      * Types defined without using this type are not allowed.
@@ -150,10 +160,26 @@ export namespace internal {
         | $.dyn
         | $.blob
 
+    type QueryParamableScalar =
+        | $.str
+        | $.bool
+        | $.int8
+        | $.uint8
+        | $.int16
+        | $.uint16
+        | $.uint32
+        | $.int32
+        | $.int64
+        | $.uint64
+        | $.float32
+        | $.float64
+        | $.timestamp
+
+    export type QueryParamable = QueryParamableScalar | $.List<QueryParamableScalar>
     export type MsgProps = { [key: string]: messagable | rpc.Msg<{ [key: string]: messagable }> }
     export type Primitive = Comparable | $.nil | $.unit
 
     // valid generic type params
-    export type Paramable = Comparable | typeRpcContainer | rpc.Msg<MsgProps>
-    export type RpcType = Primitive | typeRpcContainer | rpc.Msg<MsgProps>
+    export type Paramable = Comparable | container | rpc.Msg<MsgProps>
+    export type RpcType = Primitive | container | rpc.Msg<MsgProps>
 }
